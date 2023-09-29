043d3a58baebe29930030877b536baf141d4c75b
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-09-28 23:06:41 -0600

    added a couple queue links and link builder

--- get_data.py
+++ get_data.py
@@ -5,19 +5,29 @@ from queue import SimpleQueue
 import classes as c
 import time
 
-def buildStoryQueue():
+def buildLink(newStories=False, topStories=False, bestStories=False, item=None) -> str: 
     basic_URL = "https://hacker-news.firebaseio.com/v0/"
     suffix = ".json?print=pretty"
-    top_story_ids_json = get(basic_URL+"newstories"+suffix)
+
+    if newStories:
+        link = basic_URL+"newstories"+suffix
+    elif topStories:
+        link = basic_URL+"topstories"+suffix
+    elif not item is None:
+        link = basic_URL+'item/'+str(item)+suffix
+    else:
+        print('No option selected')
+    
+    return link
+
+
+def buildStoryQueue(newStories, topStories, bestStories) -> GeneratorExit:
+    hacker_news_link = buildLink(newStories, topStories, bestStories)
+    top_story_ids_json = get(hacker_news_link)
     top_story_ids = loads(top_story_ids_json.content)
     story_queue = SimpleQueue()
 
     for story in top_story_ids:
-        middle = 'item/'+str(story)
-        current_story_data = loads(get(basic_URL+middle+suffix).content)
+        current_story_data = loads(get(buildLink(item=story)).content)
         yield c.STORY(current_story_data["url"], current_story_data["title"], current_story_data["id"])
         # story_queue.put(story_obj)
-
-    # return story_queue
-
-buildStoryQueue()
--- website/views.py
+++ website/views.py
@@ -2,9 +2,22 @@ from flask import Blueprint, render_template, session
 from get_data import buildStoryQueue
 
 views = Blueprint('views', __name__)
-queue = buildStoryQueue()
+new_stories_queue = buildStoryQueue(newStories=True, topStories=False, bestStories=False)
+top_stories_queue = buildStoryQueue(newStories=False, topStories=True, bestStories=False)
+best_stories_queue = buildStoryQueue(newStories=False, topStories=False, bestStories=True)
 
-@views.route('/')
-def viewStory():
-    var = next(queue)
+
+@views.route('/new')
+def newStories():
+    var = next(new_stories_queue)
+    return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link)
+
+@views.route('/top')
+def topStories():
+    var = next(new_stories_queue)
+    return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link)
+
+@views.route('/best')
+def bestStories():
+    var = next(new_stories_queue)
     return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link)
