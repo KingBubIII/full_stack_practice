09150d04cb8f9902c42e8d731f8cd6c1e4f5e3b7
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-10-02 23:34:49 -0600

    add btn for database functionality later

--- website/views.py
+++ website/views.py
@@ -1,29 +1,30 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request, redirect
 from get_data import buildStoryQueue
+import os
 
 views = Blueprint('views', __name__)
 new_stories_queue = None
 top_stories_queue = None
 best_stories_queue = None
-
+current_story = None
 
 @views.route('/new')
 def newStories():
     global new_stories_queue
     if new_stories_queue is None:
         new_stories_queue = buildStoryQueue(newStories=True, topStories=False, bestStories=False)
-    var = next(new_stories_queue)
+    current_story = next(new_stories_queue)
 
-    return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link, refresh_link='/new')
+    return render_template('story_view.html', story_title=current_story.title, story_snapshot=current_story.snapshot, story_link=current_story.link, refresh_link='new', save_link='like-story')
 
 @views.route('/top')
 def topStories():
     global top_stories_queue
     if top_stories_queue is None:
         top_stories_queue = buildStoryQueue(newStories=False, topStories=True, bestStories=False)
-    var = next(top_stories_queue)
+    current_story = next(top_stories_queue)
 
-    return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link, refresh_link='/top')
+    return render_template('story_view.html', story_title=current_story.title, story_snapshot=current_story.snapshot, story_link=current_story.link, refresh_link='top', save_link='like-story')
 
 """
 @views.route('/best', methods=['GET','POST'])
@@ -31,7 +32,13 @@ def bestStories():
     global best_stories_queue
     if best_stories_queue is None:
         best_stories_queue = buildStoryQueue(newStories=False, topStories=False, bestStories=True)
-    var = next(best_stories_queue)
+    current_story = next(best_stories_queue)
 
-    return render_template('story_view.html', story_title=var.title, story_snapshot=var.snapshot, story_link=var.link, refresh_link='/best')
+    return render_template('story_view.html', story_title=current_story.title, story_snapshot=current_story.snapshot, story_link=current_story.link, refresh_link='/best')
 """
+
+@views.route('/like-story')
+def saveStory():
+    feed = request.args.get('feed', type = str)
+    print(feed)
+    return redirect("/{0}".format(feed))
